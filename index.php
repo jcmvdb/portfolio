d8a3c415f48c082806408107a42ab1683bab2ec9
jcmvdb <mvdb.jeroen@gmail.com>
2023-02-13 18:39:38 +0100

    Index

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
 
 require "header.php";
-?>
-    <h1>Bezig</h1>
-<?php
+require "functions.php";
+
+$page = "pages/home";
+if (isset($_GET["page"]) && !empty($_GET["page"])) {
+    $page = "pages/".$_GET['page'];
+}
+
+if (file_exists($page . ".php")) {
+    include($page . ".php");
+} else {
+    include "404.php";
+}
+
 require "footer.php";
