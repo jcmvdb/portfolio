11351be0e67f27189f676c8c70819cf36e5e7388
jcmvdb <mvdb.jeroen@gmail.com>
2023-03-03 21:46:09 +0100

    a beginning of the portfolio page
    this are the cards that need to be clickable and would enlarge to let you see all the details of the project and if it is a website the link and the link to the github

--- pages/portfolio.php
+++ pages/portfolio.php
@@ -1,1 +1,20 @@
-<h1>Portfolio</h1>
+<div id="portfolioWrapper">
+    <h1>Portfolio</h1>
+    <div class="projects">
+        <?php
+        $json = file_get_contents("db/data.json");
+        $data = json_decode($json, true);
+
+        foreach ($data as $dataItem) {
+            ?>
+            <div class="project">
+                <div class="heading">
+                    <img src="../assets/img/<?= $dataItem["img"]; ?>" alt="">
+                    <h1><?= $dataItem["project_name"]; ?></h1>
+                </div>
+            </div>
+            <?php
+        }
+        ?>
+    </div>
+</div>
